2979c7cc14f2a6fc52c0f05eac0649b867204148
sachinsrivastava1028 <sachinsrivastava1028@gmail.com>
2023-10-26 02:22:48 +0530

    Day13

--- Day 13-While and Do-while/month-switch.cpp
+++ Day 13-While and Do-while/month-switch.cpp
@@ -3,63 +3,19 @@ using namespace std;
 
 int main()
 {
-    int i;
-    cout<<"Enter the value of 'i': ";
-    cin>>i;
+    int n;
+    cout<<"Enter the value of 'n': ";
+    cin>>n;
 
-    switch(i)
+    int i=2;
+    while(i<=n-1)
     {
-        case 1:
-        cout<<"Jan";
-        break;
+        if(n%i==0)
+        {
+        cout<<i<<" ";
+        }
+        i=i+1;
+    }
+    
 
-        case 2:
-        cout<<"Feb";
-        break;
-
-        case 3:
-        cout<<"Mar";
-        break;
-
-        case 4:
-        cout<<"April";
-        break;
-
-        case 5:
-        cout<<"May";
-        break;
-
-        case 6:
-        cout<<"June";
-        break;
-
-        case 7:
-        cout<<"July";
-        break; 
-
-        case 8:
-        cout<<"Aug";
-        break;
-
-        case 9:
-        cout<<"Sept";
-        break;
-
-        case 10:
-        cout<<"Oct";
-        break;
-
-        case 11:
-        cout<<"Nov";
-        break;
-
-        case 12:
-        cout<<"Dec";
-        break;
-
-        default:
-        cout<<"";
-        break;
-
-    } 
 }

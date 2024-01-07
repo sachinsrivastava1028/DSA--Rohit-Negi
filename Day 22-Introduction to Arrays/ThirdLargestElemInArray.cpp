aa9b1514cecac2933c61940b2707550bb277adfe
sachinsrivastava1028 <sachinsrivastava1028@gmail.com>
2024-01-07 14:52:28 +0530

    correction day22

--- Day 22-Introduction to Arrays/ThirdLargestElemInArray.cpp
+++ Day 22-Introduction to Arrays/ThirdLargestElemInArray.cpp
@@ -5,37 +5,43 @@ using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter the number of element in array: ";
-    cin>>n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
 
-    if(n<3)
+    if (n < 3)
     {
-        cout<<"NOT VALID";
+        cout << "NOT VALID";
     }
-
     else
     {
         int arr[n];
-        cout<<"\nEnter "<<(n)<<" elements:\n";
-        for(int i=0;i<n;i++)
+        cout << "\nEnter " << n << " elements:\n";
+        for (int i = 0; i < n; i++)
         {
-            cin>>arr[i];
+            cin >> arr[i];
         }
 
-         int first, second, third;
-    first = second = third = INT_MIN;
+        int first, second, third;
+        first = second = third = INT_MIN;
 
-    for (int i=0;i<n;i++) 
-    {
-        if (arr[i] > first) {
-            first = arr[i];
-        } else if (arr[i] > second) {
-            second = arr[i];
-        } else if (arr[i] > third) {
-            third = arr[i];
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] > first)
+            {
+                third = second;
+                second = first;
+                first = arr[i];
+            }
+            else if (arr[i] > second)
+            {
+                third = second;
+                second = arr[i];
+            }
+            else if (arr[i] > third)
+            {
+                third = arr[i];
+            }
         }
-        
+        cout << "The third largest element is: " << third << endl;
     }
-    cout << "The third largest element is: " << third << endl;
-  }
 }
